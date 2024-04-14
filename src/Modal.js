524c8d693de8122ca481dbe8d9994de03e5eee28
giana94 <giana.hardin@gmail.com>
2024-04-14 16:56:27 -0400

    PortalsDemo

--- src/Modal.js
+++ src/Modal.js
@@ -1,15 +1,42 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+// export const Modal = (props) => {
+//     return (
+//         <>
+//         <div className="modal">
+//             <p>Did you complete this task?</p>
+//             <button onClick={() => props.dismissModal()} className="btn btn-highlight">Cancel</button>
+//             <button onClick={() => props.dismissModal()} className="btn">Confirm</button>
+//         </div>
+//         <div onClick={() => props.dismissModal()} className="backdrop"></div>
+//         </>
+//     );
+// };
+
+
 
 export const Modal = (props) => {
-    return (
-        <>
+    const rootModalElement = document.getElementById('root-modal');
+  
+    return ReactDOM.createPortal(
+      <>
         <div className="modal">
-            <p>Did you complete this task?</p>
-            <button onClick={() => props.dismissModal()} className="btn btn-highlight">Cancel</button>
-            <button onClick={() => props.dismissModal()} className="btn">Confirm</button>
+          <p>Did you complete this task?</p>
+          <button
+            onClick={() => {
+              props.dismissModal();
+            }}
+            className="btn btn-highlight"
+          >
+            Cancel
+          </button>
+          <button onClick={() => {}} className="btn">
+            Confirm
+          </button>
         </div>
-        <div onClick={() => props.dismissModal()} className="backdrop"></div>
-        </>
+        <div onClick={() => {}} className="backdrop" />
+      </>,
+      rootModalElement
     );
-};
-
+  };
--- src/Todo.js
+++ src/Todo.js
@@ -3,6 +3,7 @@ import {useState} from 'react';
 
 const Todo = () => {
     const [showModal, setShowModal] = useState(false);
+    
 
     const onDissmissModal = () => {
         setShowModal(false);
@@ -16,7 +17,20 @@ const Todo = () => {
                     <button onClick={() => setShowModal(true)} class="btn">Done</button>
                 </div>
             </div>
-            {showModal && <Modal dismissModal={onDissmissModal}/>}
+            {/* {showModal && <Modal dismissModal={onDissmissModal}/>} */}
+            {
+                showModal ? (
+                    <div id="first-child">
+                    <div id="second-child">
+                        <div id="third-child">
+                        <div id="fourth-child" style={{ color: 'yellow', fontSize: '2em' }}>
+                            <Modal dismissModal={onDissmissModal} />
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                ) : null 
+            }
         </>
     );
 };
